e6fa64f9688d56879596c25179795657936699bc
dtmiranda <hortadito2@gmail.com>
2023-11-25 10:25:20 -0100

    Make api first integration

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/api/v1/',
-  timeout: 5000,
+  baseURL: 'http://localhost:8080',
+
   headers: {
     'Content-Type': 'application/json'
   },
