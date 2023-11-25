63a39a2d0bb76f5e5a4f69d27293110186f10e82
dtmiranda <hortadito2@gmail.com>
2023-11-25 10:57:46 -0100

    create home page route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import api from './api/axiosConfig'
 import { useState, useEffect } from 'react'
-import Layout from './component/Layout';
+import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom'
+import Home from './components/home/Home';
 
 function App() {
 
@@ -30,7 +31,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={Layout}>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />} ></Route>
 
 
         </Route>
